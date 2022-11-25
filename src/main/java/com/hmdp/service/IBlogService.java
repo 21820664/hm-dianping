226241799af0be18c7df2e8ae7f453e427a137f1
WU <21820664@qq.com>
2022-11-25 12:00:18 +0800

    :sparkles: 达人探店

--- src/main/java/com/hmdp/service/IBlogService.java
+++ src/main/java/com/hmdp/service/IBlogService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -12,5 +13,32 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2021-12-22
  */
 public interface IBlogService extends IService<Blog> {
-
+	
+	/**
+	 * @Description 通过ID查询Blog
+	 * @Param [id]
+	 * @return com.hmdp.dto.Result
+	 */
+	Result queryBlogById(Long id);
+	
+	/**
+	 * @Description 通过排名查询热门博客
+	 * @Param [current]
+	 * @return com.hmdp.dto.Result
+	 */
+	Result queryHotBlog(Integer current);
+	
+	/**
+	 * @Description 点赞博客
+	 * @Param [id]
+	 * @return com.hmdp.dto.Result
+	 */
+	Result likeBlog(Long id);
+	
+	/**
+	 * @Description 查询博客最早点赞人
+	 * @Param [id]
+	 * @return com.hmdp.dto.Result
+	 */
+	Result queryBlogLikes(Long id);
 }
--- src/main/java/com/hmdp/utils/SystemConstants.java
+++ src/main/java/com/hmdp/utils/SystemConstants.java
@@ -1,7 +1,8 @@
 package com.hmdp.utils;
 
 public class SystemConstants {
-    public static final String IMAGE_UPLOAD_DIR = "D:\\lesson\\nginx-1.18.0\\html\\hmdp\\imgs\\";
+    //写死了,需根据代码路径改变
+    public static final String IMAGE_UPLOAD_DIR = "D:\\Programme\\Projects\\Java\\hmdp\\nginx-1.18.0\\html\\hmdp\\imgs";
     public static final String USER_NICK_NAME_PREFIX = "user_";
     public static final int DEFAULT_PAGE_SIZE = 5;
     public static final int MAX_PAGE_SIZE = 10;
